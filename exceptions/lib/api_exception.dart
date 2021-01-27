12212794b73fa12819d643a109e084f81e43607c
Edwin Nyawoli <edwin.nyawoli@affinitylabsafrica.com>
2021-01-27 15:17:03 +0000

    Provide reason for exception

--- exceptions/lib/api_exception.dart
+++ exceptions/lib/api_exception.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 class ApiException implements Exception {
   int code = 0;
   String message;
@@ -18,5 +20,36 @@ class ApiException implements Exception {
 
     return "ApiException $code: $message (Inner exception: $innerException)\n\n" +
         stackTrace.toString();
+  }
+
+  ///
+  /// Provide reason for exception based on http status code value.
+  /// This provides a reason for only a few common status codes.
+  ///
+  String reason({String resource = 'resource'}) {
+    switch (code) {
+      // 2xx
+      case HttpStatus.ok:
+        return 'Your request has been processed successfully';
+      case HttpStatus.created:
+        return 'The $resource has been created successfully';
+
+      // 4xx
+      case HttpStatus.badRequest:
+        return "Your request couldn't be processed. Please try again";
+      case HttpStatus.unauthorized:
+        return 'Please provide your credentials to perform this operation';
+      case HttpStatus.forbidden:
+        return 'Forbidden';
+      case HttpStatus.notFound:
+        return 'The requested $resource was not found';
+
+      // 5xx
+      case HttpStatus.internalServerError:
+        return "Something went wrong on our side. We're working to fix it.";
+
+      default:
+        return message;
+    }
   }
 }
