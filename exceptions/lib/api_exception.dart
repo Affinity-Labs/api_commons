08ba2b7b32226155618bad4cd1f4ec0dea20e796
Edwin Nyawoli <edwin.nyawoli@affinitylabsafrica.com>
2021-09-29 16:22:24 +0000

    Add custom error message for gateway timeout errors

--- exceptions/lib/api_exception.dart
+++ exceptions/lib/api_exception.dart
@@ -94,6 +94,9 @@ class ApiException implements Exception {
       case HttpStatus.internalServerError:
         return "Something went wrong on our side. We’re working to fix it.";
 
+      case HttpStatus.gatewayTimeout:
+        return "We're experiencing difficulties reaching Affinity. Please try again.";
+
       default:
         return message;
     }
