ebe83d5238e3f13327abc6b7d31f92b1519bb2e2
Edwin Nyawoli <edwin.nyawoli@affinitylabsafrica.com>
2022-10-31 11:17:04 +0000

    Return a future from applyToParams

--- auth/lib/authentication.dart
+++ auth/lib/authentication.dart
@@ -11,8 +11,8 @@
 // ignore: one_member_abstracts
 import 'package:auth/query_param.dart';
 
+// ignore: one_member_abstracts
 abstract class Authentication {
   /// Apply authentication settings to header and query params.
-  void applyToParams(
-      List<QueryParam> queryParams, Map<String, String> headerParams);
+  Future<void> applyToParams(List<QueryParam> queryParams, Map<String, String> headerParams);
 }
